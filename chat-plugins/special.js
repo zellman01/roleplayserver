1d7ba3843f24fdeb055964fa747fcec2f72e74d0
zellman01 <18561222+zellman01@users.noreply.github.com>
2018-09-15 07:34:32 -0400

    special: Add voice in group section

--- chat-plugins/special.js
+++ chat-plugins/special.js
@@ -8,18 +8,21 @@ exports.commands = {
 		switch (target) {
 		case "groups":
 			this.sendReplyBox(
-				`These are the only ranks used in official rooms on this server<br />` +
+				`These are the only ranks used in official rooms on this server, with the exception of Lobby<br />` +
 				`\u2605 <strong>Room Host</strong> - The person currently hosting an RP in that specific room.<br /><br />` +
 				`# <strong>Room Owner</strong> - Only given to the bot (except in major cases).<br />` +
 				`<br />` +
-				`<strong>Global Staff</strong><br />` +
+				`<strong>Globals</strong><br />` +
+				`+ <strong>Voice</strong> - These users are not staff members.<br />` +
+				`<strong>Voices</strong> are exceptional users. The best way to get this is either doing a major contribution to the ` +
+				`server, or being an exceptional roleplayer.<br /><br />`
 				`% <strong>Driver</strong> - These are staff members under a trial, so you can call them in a sense a trial moderator.<br />` +
 				`<strong>Drivers</strong> have some control of the bot, but not much, as well as all of the driver server commands.<br /><br />` +
 				`@ <strong>Moderator</strong> - These are the lowest an actual staff member may be.<br />` +
 				`<strong>Moderators</strong> have more control over the bot then Drivers, and have mod server commands.<br /><br />` +
-				`& <strong>Leaders</strong> - Lowest upper staff member.<br />` +
+				`& <strong>Leader</strong> - Lowest upper staff member.<br />` +
 				`<strong>Leaders</strong> have almost full control of the bot, and can use leader server commands.<br /><br />` +
-				`~ <strong>Administrators</strong> - Server owners.<br />` +
+				`~ <strong>Administrator</strong> - Server owners.<br />` +
 				`<strong>Administartors</strong> have full control of the bot, and the server.`
 			);
 			break;
