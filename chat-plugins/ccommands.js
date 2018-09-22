52c38d63d56a53ccd91e0d73818dc622433068e4
zellman01 <18561222+zellman01@users.noreply.github.com>
2018-09-22 17:12:03 -0400

    auth: Remove GVoice from list

--- chat-plugins/ccommands.js
+++ chat-plugins/ccommands.js
@@ -35,7 +35,7 @@ exports.commands = {
 		let ranks = Object.keys(Config.groups);
 		for (let u in Users.usergroups) {
 			let rank = Users.usergroups[u].charAt(0);
-			if (rank === ' ') continue;
+			if (rank === ' ' || rank === '+') continue;
 			// In case the usergroups.csv file is not proper, we check for the server ranks.
 			if (ranks.includes(rank)) {
 				let name = Users.usergroups[u].substr(1);
